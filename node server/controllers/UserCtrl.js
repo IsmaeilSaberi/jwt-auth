788d54a02d0e45a585f9cf40a500056799991a92
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-01-28 09:42:57 -0800

    I worked on createuser controller for checking the formdata info with regex!

--- node server/controllers/UserCtrl.js
+++ node server/controllers/UserCtrl.js
@@ -24,6 +24,38 @@ module.exports.getOneUser = getOneUser;
 
 const createUser = async (req, res) => {
   try {
+    if (
+      !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,30}$/.test(req.body.username)
+    ) {
+      return res.status(401).json({ msg: "username structure is wrong!" });
+    }
+    if (
+      !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,30}$/.test(
+        req.body.displayname
+      )
+    ) {
+      return res.status(401).json({ msg: "displayname structure is wrong!" });
+    }
+    if (
+      !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(req.body.email)
+    ) {
+      return res.status(401).json({ msg: "email structure is wrong!" });
+    }
+    if (!/^[0][0-9]{10}$/.test(req.body.phone)) {
+      return res.status(401).json({ msg: "phone structure is wrong!" });
+    }
+    if (
+      !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,24}$/.test(
+        req.body.password
+      )
+    ) {
+      return res.status(401).json({ msg: "password structure is wrong!" });
+    }
+    if (formData.password != formData.repassword) {
+      return res
+        .status(401)
+        .json({ msg: "repassword is not match with password!" });
+    }
   } catch (error) {
     console.log(err);
     res.status(400).json({ msg: "an error in creating user!" });
